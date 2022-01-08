8383c9aec438f473e48dba52430e00bbe0e049a3
enesgokk <enes_sare@hotmail.com>
2022-01-08 22:23:43 +0300

    wearekazan

---
+++ js/settings.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#scrollDiv").scrollLeft(72);
+});
+
+$('body').mousewheel(function(event) {
+    console.log(event.deltaX, event.deltaY, event.deltaFactor);
+    
+    if(event.deltaY===1){
+        $("#scrollDiv").scrollLeft(-400);
+    }
+    else if(event.deltaY===-1){
+        $("#scrollDiv").scrollLeft(2000);
+    }
+});
+
+document.addEventListener('scroll', function (e) {     
+    console.log('scrolling', window.scrollY);
+    if(window.scrollY>800){
+        $("#tab1").text("")
+        $("#tab2").text("Fintech")
+        $("#tab1").css("color","rgb(209 213 219)")
+        $("#tab1Index").css("color","rgb(209 213 219)")
+        $("#tab2").css("color","white")
+        $("#tab2Index").css("color","white")
+    }
+    else{
+        $("#tab1").text("Branding")
+        $("#tab2").text("") 
+        $("#tab2").css("color","rgb(209 213 219)")
+        $("#tab2Index").css("color","rgb(209 213 219)")
+        $("#tab1").css("color","white")
+        $("#tab1Index").css("color","white")
+    }
+}, true);
